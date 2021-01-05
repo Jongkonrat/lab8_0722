a0d36aee71a6b60fe1478f1ba1b43ef0b3d75850
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-01-05 16:19:31 +0700

    finish

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M){
+	if(N<=0 || M<=0){
+		cout << "Invalid input";
+	}
+	else{
+		for(int x=0; x<N; x++){
+			for(int y=0; y<M; y++){
+				cout << "O";
+			}
+		cout << "\n";
+		}
+	}
+}
 
 int main(){
 	
@@ -18,7 +31,5 @@ int main(){
 	cout << "\n";	
 	
 	printO(7,-1);
-	cout << "\n";	
-	
 	return 0;
 }
